f5b6b0064424875f53d4fd96e9af7a69e73468bd
Carlton Joseph <carlton.joseph@gmail.com>
2022-06-21 18:39:20 -0700

    add random pokemon fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,20 @@
 import type { NextPage } from "next";
-import { trpc } from "@/utils/trpc";
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
 
 const Home: NextPage = () => {
-  const { data, isLoading } = trpc.useQuery(["hello", { text: "Carlton" }]);
-
-  if (isLoading) return <div>Loading</div>;
-  if (data) return <div>{data.greeting}</div>;
+  const [first, second] = getOptionsForVote();
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
       <div className="p-2"></div>
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800" id="1">
+          {first}
+        </div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800" id="2">
+          {second}
+        </div>
       </div>
     </div>
   );
