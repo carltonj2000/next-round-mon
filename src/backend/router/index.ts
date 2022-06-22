cf156bea9fd905a906162a94cfc72373881c4dbd
Carlton Joseph <carlton.joseph@gmail.com>
2022-06-21 20:01:48 -0700

    style buttons and fix trpc cors

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -8,7 +8,7 @@ export const appRouter = trpc.router().query("get-pokemon-by-id", {
   async resolve({ input }) {
     const api = new PokemonClient();
     const pokemon = await api.getPokemonById(input.id);
-    return pokemon;
+    return { sprites: pokemon.sprites, name: pokemon.name };
   },
 });
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import type { NextPage } from "next";
 import { trpc } from "@/utils/trpc";
 import { getOptionsForVote } from "@/utils/getRandomPokemon";
 
+const btn =
+  "mt-2 inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+
 const Home: NextPage = () => {
   const [ids, idsSet] = useState(() => getOptionsForVote());
   const [first, second] = ids;
@@ -11,12 +14,17 @@ const Home: NextPage = () => {
 
   if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
 
+  const voteForRoundest = (selected: number) => {
+    // todo: fire mutation to persist changes
+    idsSet(getOptionsForVote());
+  };
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
       <div className="p-2"></div>
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-64 h-64 flex flex-col items-center">
+        <div className="w-64 h-64 flex flex-col items-center justify-center">
           <img
             src={firstPokemon.data?.sprites.front_default}
             className="w-full"
@@ -24,9 +32,12 @@ const Home: NextPage = () => {
           <div className="text-xl capitalize mt-[-2rem]">
             {firstPokemon.data?.name}
           </div>
+          <button onClick={() => voteForRoundest(first)} className={btn}>
+            Rounder
+          </button>
         </div>
         <div className="p-8">Vs</div>
-        <div className="w-64 h-64 flex flex-col items-center">
+        <div className="w-64 h-64 flex flex-col items-center justify-center">
           <img
             src={secondPokemon.data?.sprites.front_default}
             className="w-full"
@@ -34,6 +45,9 @@ const Home: NextPage = () => {
           <div className="text-xl capitalize mt-[-2rem]">
             {secondPokemon.data?.name}
           </div>
+          <button onClick={() => voteForRoundest(second)} className={btn}>
+            Rounder
+          </button>
         </div>
       </div>
     </div>
